118b22582ab486fff8d8720b0d2d1dac39c91ed2
TheSprtCz <halapetr@selfnet.cz>
2019-11-24 11:25:05 +0100

    In-Memory caching

--- chords/settings.py
+++ chords/settings.py
@@ -143,6 +143,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'chords',
+    }
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
