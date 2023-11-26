b039e470a72705d037249795959b0a99d1f5c040
Petr Hála <halapetr@selfnet.cz>
2023-11-26 18:50:11 +0100

    Add version to options menu
    - Move tenants.utils to backend

--- backend/templatetags/utils.py
+++ backend/templatetags/utils.py
@@ -2,6 +2,7 @@
 from django import template
 from django.template.defaultfilters import stringfilter
 
+from backend import utils
 from tenants.utils import create_tenant_string
 
 register = template.Library()
@@ -20,3 +21,15 @@ def draw_menu(context, key, use_tenant=False):
 def prefix(value, args):
     """Adds prefix if value is not empty"""
     return str(args) + value if value else value
+
+
+@register.simple_tag
+def version():
+    """Returns currently used version e.g. date of the last commit"""
+    return utils.get_version()
+
+
+@register.simple_tag
+def git_revision():
+    """Returns sha of the latest revision"""
+    return utils.get_git_revision()
