71f54d9d88db75e8d1cfe2c29f8d8057d2465458
Petr Hála <halapetr@selfnet.cz>
2023-11-26 18:04:15 +0100

    Only add <hr> to options when they are any

--- tenants/templatetags/utils.py
+++ tenants/templatetags/utils.py
@@ -1,5 +1,6 @@
 """Tags for drawing the menu"""
 from django import template
+from django.template.defaultfilters import stringfilter
 
 from tenants.utils import create_tenant_string
 
@@ -12,3 +13,10 @@ def draw_menu(context, key, use_tenant=False):
     if use_tenant:
         key = create_tenant_string(context.request.tenant, key)
     return {"menu": context["menus"][key]}
+
+
+@register.filter
+@stringfilter
+def prefix(value, args):
+    """Adds prefix if value is not empty"""
+    return str(args) + value if value else value
