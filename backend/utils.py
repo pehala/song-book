11cf0403eb0b2db818f67ced97d4be5e3225d725
Petr Hála <halapetr@selfnet.cz>
2023-11-26 18:50:11 +0100

    Add version to options menu
    - Move tenants.utils to backend

--- backend/utils.py
+++ backend/utils.py
@@ -1,4 +1,8 @@
 """Utility functions for backend app"""
+import subprocess
+from email.utils import parsedate_to_datetime
+from functools import cache
+
 from django.conf import settings
 
 from pdf.utils import request_pdf_regeneration
@@ -15,3 +19,18 @@ def regenerate_prerender(song):
     """Regenerates prerendered html for song"""
     if settings.USE_PRERENDERED_MARKDOWN:
         song.prerender_all()
+
+
+@cache
+def get_version():
+    """Returns currently used version e.g. date of the last commit"""
+    date = parsedate_to_datetime(
+        subprocess.check_output(["git", "--no-pager", "log", "-1", "--format='%aD'"]).decode("ascii").strip()
+    )
+    return date.strftime("%Y/%m/%d")
+
+
+@cache
+def get_git_revision():
+    """Returns sha of the latest revision"""
+    return subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).decode("ascii").strip()
