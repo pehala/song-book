f2127e563063ba11bc52d25f9c97b98e8881973c
Petr Hála <halapetr@selfnet.cz>
2024-12-20 14:48:25 +0100

    Add Media urls and debug toolbar only when Debug=true

--- chords/urls.py
+++ chords/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-import debug_toolbar
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -30,5 +29,12 @@ urlpatterns = [
     path("tenants/", include(("tenants.urls", "tenants"), namespace="tenants")),
     re_path(r"^", include(("backend.urls", "backend"), namespace="chords")),
     path("admin/", admin.site.urls),
-    path("__debug__/", include(debug_toolbar.urls)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+        *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+    ]
